4fc14bfea01c916a4867868557a9cd0f92552771
fabien-prog <fabi.gaudreau@gmail.com>
2025-08-08 12:29:48 -0400

    Force Chakra UI to always start in dark mode

--- 3-frontend/src/App.js
+++ 3-frontend/src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Box, Flex, useColorMode } from "@chakra-ui/react";
 import MainTabs from "./components/MainTabs";
 
 function App() {
+  const { setColorMode } = useColorMode();
+
+  useEffect(() => {
+    setColorMode("dark"); // Force dark mode on mount
+  }, [setColorMode]);
+
   return (
-    <Flex justify="center" align="start"  overflowY='hidden' h="100vh">
+    <Flex justify="center" align="start" overflowY="hidden" h="100vh">
       <Box maxW="100vw" minW="600px" maxH="1024px" w="100%">
         <MainTabs />
       </Box>
